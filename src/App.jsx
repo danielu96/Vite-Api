64ff194f0a4c7a59c1a6dee38563f30a045c3306
Dniel K <danielu96@wp.pl>
2023-06-16 13:24:03 +0200

    Vite + styled-components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+// import styled from 'styled-components'
+import Wrapper from './assets/wrappers/App'
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <Wrapper>
+    <div>
+    <p>API </p>
+    </div>      
+      </Wrapper>
+   
+  )
+}
+// const Wrapper = styled.div`
+//   text-align: center;
+// `
+
+
+export default App
---
+++ src/assets/wrappers/App.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+width:100vw;
+height:100vh;
+display:grid;
+background: lightGray;
+text-align: center;
+`
+
+export default Wrapper
