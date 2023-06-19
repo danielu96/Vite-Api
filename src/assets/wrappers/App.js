75ff64cb25c8f856fba7b0e844f86ed87932da07
Dniel K <danielu96@wp.pl>
2023-06-19 12:37:39 +0200

    add some style

--- src/assets/wrappers/App.js
+++ src/assets/wrappers/App.js
@@ -6,6 +6,36 @@ height:100vh;
 display:grid;
 background: lightGray;
 text-align: center;
+justify-items:center;
+button{
+  width:4rem;
+  color:black;
+  margin:3rem auto auto ;
+  background:white;
+  border-radius:5px;
+
+}
+button:hover{
+  color:white;
+  background:black;
+}
+.container{
+  display:grid;
+  justify-items:space-between;  
+  padding:1rem;
+  width:30vw;
+  height:30vh;
+  background-color:white;
+  border:solid 1px gray;
+  border-radius:10px;
+  margin-bottom:2rem;
+
+}
+.container:hover{
+  box-shadow: 2px 2px 2px 2px gray;
+transition:1s;
+
+}
 `
 
 export default Wrapper
