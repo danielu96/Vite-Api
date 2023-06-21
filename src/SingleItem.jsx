d3537a23522a6f42ca188d08fd2a49c821ddc2bb
Dniel K <danielu96@wp.pl>
2023-06-21 18:01:23 +0200

    Link added

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SingleItem = ({autor,title}) => {
+  // const {title}=useParams();
   return (
-    <div>
-        <h1>{autor}</h1>
-        <p>{title}</p>
+    <div style={{display:'flex',justifyContent:'space-between'}}>
+        <h1>{title}</h1>
+        <p>{autor}</p>
+     {/* <a href='Item' >  <button >details</button> </a> */}
+        <Link  
+        to='/Item'
+        //  to={`/Item/${title}`} 
+          > Zobacz</Link>
+
     </div>
   )
 }
