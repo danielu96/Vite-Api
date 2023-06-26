e63470cc7f3dc381cb8d5b0bf96dbe19ce7bf0a6
Dniel K <danielu96@wp.pl>
2023-06-26 12:57:05 +0200

    useParams added

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,28 +1,52 @@
 import React from 'react'
 import Wrapper from './assets/wrappers/App'
-import { useParams, useNavigate } from 'react-router-dom';
-const Item = () => {
+// import {  useEffect,useState } from 'react'
+import { useParams, useNavigate,useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+// const url = "https://rickandmortyapi.com/api/character/";
+// const localData = "../DATA/data.json" ;
+// const CoctailUrl='www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+// 'https://www.thecoctaildb.com/api/json/v1/1/lookup.php?i='
+// ;
+
+
+const Item = ({id,autor,data}) => {
+//  const {data,setData} =useState([]);
+//  const localData = "../DATA/data.json" ;
+
+//  const CoctailUrl = data.drinks[0];
+//  console.log(CoctailUrl)
+//  const url = data.character[0];
+//  console.log(character)
   const navigate = useNavigate();
   const handleOnClick = () => navigate(-1);
-//     const {title} = useParams();
-//      const item = data.find((item) => item.title ===
-// title);
+   const {title}=useParams();
+  
+//          const item = data.find((item) => item.title ===
+//  title);
+ 
+
   return (
     <>
-    <Wrapper>
+   
     <div>
-      <h1>SHOW</h1>
+      <h5>SHOW</h5>
+     <h1>{title}</h1> 
     {/* {title} */}
-    </div>
+    <p>{autor}</p>
+    </div>   
     <button onClick={handleOnClick}>back</button>
-      {/* {item
+      {/* {
+      
+      data
         .filter((item) => item.title === title)
+
         .map((item, id) => (
-            <div key={id}>{item.title} </div>
+            <div key={id}>{item.autor} </div>
    
   ))
         } */}
-    </Wrapper>
+    
   
  </>
   )
