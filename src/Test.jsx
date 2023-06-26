44bec26094d4103c4054de0f6bcd59eb4b050048
Dniel K <danielu96@wp.pl>
2023-06-26 12:50:33 +0200

    Test.jsx created

---
+++ src/Test.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+import CoctailList from './CoctailList';
+
+const CoctailFullUrl="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+   
+
+
+export const loader = async ()=>{
+    const searchTerm="";
+    const response= await axios.get(`${CoctailFullUrl}${searchTerm}`);
+    console.log(response)
+    return {drinks:response.data.drinks, searchTerm}
+  };
+
+const Test = () => {
+  const {drinks,searchTerm}=useLoaderData();
+  console.log(drinks);
+  return (
+    <div >
+      <CoctailList drinks={drinks}/>
+        
+    </div>
+  )
+}
+
+export default Test
