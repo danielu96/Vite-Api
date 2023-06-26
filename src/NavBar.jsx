61260ff9dc55f6e827d67d38ebde2ad32eca4ccc
Dniel K <danielu96@wp.pl>
2023-06-26 12:50:10 +0200

    NavLink to Test added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div style={{width:'100vw',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem" }}>
+    <div style={{width:'100vw',height:'5vh',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem" }}>
    Navbar
     <NavLink to="/">Home</NavLink>
     <NavLink to="/List">List</NavLink>
-    <NavLink to="/Item">Details</NavLink>
+    <NavLink to="/Test">Test</NavLink>
     
     </div>
   )
