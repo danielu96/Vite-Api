12bc4f9176f3b2f0ebe3528f583cc50bfd3491d7
Dniel K <danielu96@wp.pl>
2023-06-28 21:12:03 +0200

    fetchData added

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
-import Wrapper from './assets/wrappers/App'
-// import {  useEffect,useState } from 'react'
-import { useParams, useNavigate,useLoaderData } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import {  useEffect,useState } from 'react'
+// import { useLoaderData } from 'react-router-dom';
+// import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import Wrapper from './assets/wrappers/App';
+// const CoctailUrl='https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+ const localData = "../DATA/data.json" ;
+
+
+// const singleCocktailQuery = (id) => {
+//   return {
+//     queryKey: ['cocktail', id],
+//     queryFn: async () => {
+//       const { data } = await axios.get(`${localData}${id}`);
+//       return data;
+//     },
+//   };
+// };
+
+// export const loader =
+//   (queryClient) =>
+//   async ({ params }) => {
+//     const { id } = params;
+//     await queryClient.ensureQueryData(singleCocktailQuery(id));
+//     return { id };
+//   };
+
+// export const loader = async ({params})=>{
+//   const {id}=params;
+//   const {data}= await axios.get(`${CoctailUrl}${id}`);
+//   console.log(data)
+//   return {id,data}
+// };
+
+const Item = () => {
+  const [data,setData] = useState();
+  const {title}=useParams();
+  const fetchData = async () =>{
+    try{
+      const response = await axios(localData
+      
+        );
+      const data= response.data;
+   setData(data)
+      // console.log(data);
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
+// const { id}=useLoaderData();
+// const { data } = useQuery(singleCocktailQuery(id));
+// const singleItem = data.drinks[0];
+// const {strDrink:name}=SingleDrink;
+// const {
+//   title: title,
+// } = singleItem;
+  return (
+<Wrapper>    <h1> {title}</h1>   
+
+{/* {data
+.filter((item)=>(item.title===title))
+.map((item,id)=>(
+         <div key={id}>  
+     {item.title}
+      </div>  
+     ) )}  */}
+
+ </Wrapper> 
+
+  
+   
+       
+  
+    
+  )
+}
+
+export default Item
+
+
+
+// import React from 'react'
+// import Wrapper from './assets/wrappers/App'
+// // import {  useEffect,useState } from 'react'
+// import { useParams, useNavigate,useLoaderData } from 'react-router-dom';
+// import axios from 'axios';
 // const url = "https://rickandmortyapi.com/api/character/";
 // const localData = "../DATA/data.json" ;
 // const CoctailUrl='www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
@@ -10,46 +96,31 @@ import axios from 'axios';
 // ;
 
 
-const Item = ({id,autor,data}) => {
-//  const {data,setData} =useState([]);
-//  const localData = "../DATA/data.json" ;
+// const Item = ({id,autor,data}) => {
 
-//  const CoctailUrl = data.drinks[0];
-//  console.log(CoctailUrl)
-//  const url = data.character[0];
-//  console.log(character)
-  const navigate = useNavigate();
-  const handleOnClick = () => navigate(-1);
-   const {title}=useParams();
+//   const navigate = useNavigate();
+//   const handleOnClick = () => navigate(-1);
+//    const {title}=useParams();
   
-//          const item = data.find((item) => item.title ===
-//  title);
+// //          const item = data.find((item) => item.title ===
+// //  title);
  
 
-  return (
-    <>
+//   return (
+//     <>
    
-    <div>
-      <h5>SHOW</h5>
-     <h1>{title}</h1> 
-    {/* {title} */}
-    <p>{autor}</p>
-    </div>   
-    <button onClick={handleOnClick}>back</button>
-      {/* {
-      
-      data
-        .filter((item) => item.title === title)
-
-        .map((item, id) => (
-            <div key={id}>{item.autor} </div>
+//     <div>
+//       <h5>SHOW</h5>
+//      <h1>{title}</h1> 
    
-  ))
-        } */}
+//     <p>{autor}</p>
+//     </div>   
+//     <button onClick={handleOnClick}>back</button>
+   
     
   
- </>
-  )
-}
+//  </>
+//   )
+// }
 
-export default Item
+// export default Item
