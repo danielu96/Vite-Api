c857cc91b5cf9263e8b5198462ae76b5c5a5e83f
Dniel K <danielu96@wp.pl>
2023-06-28 21:03:59 +0200

    ToDo.jsx created

---
+++ src/Pages/ToDo.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+const localData = "../DATA/data.json" ;
+
+const singleToDoQuery = (id) => {
+    return {
+      queryKey: ['todo', id],
+      queryFn: async () => {
+        const { data } = await axios.get(`${localData}${id}`);
+        console.log({data})
+        return data;
+      },
+    };
+  };
+  
+  export const loader =
+    (queryClient) =>
+    async ({ params }) => {
+      const { id } = params;
+      await queryClient.ensureQueryData(singleToDoQuery(id));
+      return { id };
+    };
+
+const ToDo = () => {
+    const { id}=useLoaderData();
+const { data } = useQuery(singleToDoQuery(id));
+const singleToDo = data.items[0];
+const {
+  
+    title:title,
+    autor:autor,
+   
+  } = singleToDo;
+  return (
+    <div>ToDo
+        {id}
+    </div>
+  )
+}
+
+export default ToDo
