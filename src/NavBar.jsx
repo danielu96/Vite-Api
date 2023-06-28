30393d80d0a15ce31af2359cc71533bad49a3b8e
Dniel K <danielu96@wp.pl>
2023-06-28 21:09:27 +0200

    Todos added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,8 +6,9 @@ const NavBar = () => {
     <div style={{width:'100vw',height:'5vh',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem" }}>
    Navbar
     <NavLink to="/">Home</NavLink>
-    <NavLink to="/List">List</NavLink>
+    <NavLink to="/Todos">Todos</NavLink>
     <NavLink to="/Test">Test</NavLink>
+    <NavLink to="/List">List</NavLink>
     
     </div>
   )
