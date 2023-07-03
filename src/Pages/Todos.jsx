5f3cebf2eb1b0e9f2c56176de12c5745b66ee5a4
Dniel K <danielu96@wp.pl>
2023-07-03 11:44:27 +0200

    loader fixed

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import TodoList from '../Component/TodoList'
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
-const localData = "../DATA/data.json" ;
+const localData = 
+'http://localhost:5173/DATA/db.json'
+// "../DATA/db.json"
+ ;
 
 export const loader = async ()=>{
     // const searchTerm="";
-    const response= await axios.get(localData);
+    const response= await axios.get(`${localData}`);
     console.log(response)
-    return {items:response.data}
+    return {items:response.data.asks}
   };
 
 
