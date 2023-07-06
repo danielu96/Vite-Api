677cc44c6fe140f5fc639714f18ff8e9835de684
Dniel K <danielu96@wp.pl>
2023-07-06 13:51:06 +0200

    change name SingleItem to Single

---
+++ src/Single.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Wrapper from './assets/wrappers/App'
+
+const Single = ({title}) => {
+ 
+  return (
+    
+    <div style={{width:'30vw',display:'flex',justifyContent:'space-between'}}>
+      
+        <h4>{title}</h4>
+        {/* <p>{autor}</p> */}
+        <Link  
+        // to='/Item'
+         to={`/Item/${title}`} 
+          > Zobacz</Link>
+       
+
+    </div>
+    
+  )
+}
+
+export default Single
