bb44ec1748e4b04309e068e2184cf5acf4ebdf69
Dniel K <danielu96@wp.pl>
2023-07-11 12:32:26 +0200

    add  section-enter

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Form from '../Component/Form'
-import Items from '../Component/Items'
+import Form from '../Components/Form'
+import Items from '../Components/Items'
 import { ToastContainer } from 'react-toastify'
 
 const Api = () => {
   return (
-    <div>
+    <div className='section-center'>
          <ToastContainer position='top-center' />
     
       <Form/>
