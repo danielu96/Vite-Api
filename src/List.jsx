8275a96961b5955355c7a11e6d43ca4b50fb6c8f
Dniel K <danielu96@wp.pl>
2023-07-11 15:30:52 +0200

    add  'http://localhost:5000/api/tasks'

--- src/List.jsx
+++ src/List.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-// import { Link, Outlet } from 'react-router-dom'
-import {  useEffect,useState } from 'react'
-import Wrapper from './assets/wrappers/App'
+import React from 'react';
+import {  useEffect,useState } from 'react';
+import Wrapper from './assets/wrappers/App';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { useMutation,useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Single from './Single';
 import {nanoid} from 'nanoid';
-// import Items from './Items';
+
 
 
 
 const List = () => {
-    const [data, setData] = useState([]);
+  
+    // const [data, setData] = useState([]);
     const [title , setTitle] = useState('');  
     const [autor , setAutor] = useState('');  
     // const url = "https://rickandmortyapi.com/api/character/1,10,15";
@@ -30,51 +30,54 @@ const List = () => {
     // }; 
  
     const customFetch = axios.create({
-      baseURL: 
-      // "../DATA/data.json" 
-      // 'http://localhost:5000/api/tasks'
-      'http://localhost:5173/DATA/db.json'
-      ,
+      baseURL:      
+      'http://localhost:5000/api/tasks'   ,
     });
     
  
-  const localData =
-  // "../DATA/data.json" ;
+  // const localData =
+  // // "../DATA/data.json" ;
   // "http://localhost:5000/api/tasks";
-  // 'http://localhost:5173/api/items'
-  'http://localhost:5173/DATA/db.json'
 
-const fetchData = async () =>{
-  try{
-    const response = await axios(localData
-    
-      );
-    const data= response.data.asks;
- setData(data)
-    // console.log(data);
-  } catch (error) {
-    console.log(error.response)
-  }
+ const {data} = useQuery ({
+queryKey:['tasks'],
+queryFn:async () => {
+  const {data} = await customFetch.get("/");
+  return data;
+},
 }
-useEffect(() => {
-  fetchData(); 
-}, [])
+ )
+// const fetchData = async () =>{
+//   try{
+//     const response = await axios(localData    
+//       );
+//     const data= response.data.taskList;
+//  setData(data) 
+//   } catch (error) {
+//     console.log(error.response)
+//   }
+// }
+// useEffect(() => {
+//   fetchData(); 
+// }, [])
+
 const {mutate:createTask}=useMutation({
   mutationFn:()=> customFetch.post('/', 
-  // {body: JSON.stringify(data)},
-  {body:{title:title,autor:autor,id:nanoid()}}
-  // {data:{title:title,autor:autor}}
+  {title:title,autor:autor,id:nanoid(),
+}  
   ),
 
 });
 
-
-
-
-
    const handleSubmit = async (e) =>{
-    e.preventDefault();
-    createTask();
+    e.preventDefault(); 
+    createTask(title && autor, {
+      onSuccess: () => {
+        setTitle('');
+        setAutor('');
+      },
+    });
+    // createTask(title,autor);
     // try{
     //   const resp = await axios.post (localData, {id:'4',title:title,autor:autor});
     // //  setData = JSON.stringify
@@ -92,20 +95,16 @@ const {mutate:createTask}=useMutation({
     // } 
       // return (createTask)     
   }
-
-
   return (
     <>
-    <Wrapper>   
-    
+    <Wrapper>      
     <ToastContainer 
      position="top-center"
      />
     <div className='container'>Home
     <form style={{display:'grid',alignContent:'center', justifyContent:'center'}}
     onSubmit={handleSubmit}>
-      <label style={{margin:'auto auto 7px 0px'}} htmlFor='title'>Title</label>
-     
+      <label style={{margin:'auto auto 7px 0px'}} htmlFor='title'>Title</label>     
       <input 
       style={{background:'#f7f7f7',border:'solid 1px',borderRadius:"5px"}}
       type='text'
@@ -120,21 +119,17 @@ const {mutate:createTask}=useMutation({
       id='autor'
       value={autor}
       onChange={(e)=>setAutor( e.target.value)}
-      ></input>    
-          
+      ></input>            
    <button onClick={handleSubmit}>check it</button>
      </form>      
     </div>    
-    {data.map((item,id)=>(
+    {data.taskList.map((item,id)=>(
          <div key={id}>  
-    <Single title={item.title} autor={item.autor}/>
-       
+    <Single title={item.title} autor={item.autor}/>       
         </div>
-      ))}    
-    
+      ))}        
     </Wrapper>
     </>
   )
 }
-
 export default List
