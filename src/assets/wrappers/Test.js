4c034f09639589d6fbaba9afee7502c9cf96264b
Dniel K <danielu96@wp.pl>
2023-07-11 15:26:35 +0200

    created file

---
+++ src/assets/wrappers/Test.js
@@ -0,0 +1,21 @@
+// import styled from 'styled-components'
+
+// const Wrapper = styled.div`
+
+// display:flex;
+// background: lightBlue;
+// grid-template-columns: repeat(auto-fill, minmax(200px,1fr));
+// gap:2rem;
+// `;
+
+// export default Wrapper
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+width:80vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+`;
+
+export default Wrapper;
