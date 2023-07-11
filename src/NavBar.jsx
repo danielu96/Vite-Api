9850ed35f3da047e3de4efacbc27fd2cb219f82e
Dniel K <danielu96@wp.pl>
2023-07-11 15:31:18 +0200

    add Newsletter

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,13 +3,14 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div style={{width:'100vw',height:'5vh',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem" }}>
-   Navbar
+    <div style={{width:'100vw',height:'5vh',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem",paddingTop:'0.5rem' }}>
+  
     <NavLink to="/">Home</NavLink>
-    <NavLink to="/Todos">Todos</NavLink>
+    <NavLink to="/tasks">Todos</NavLink>
     <NavLink to="/Test">Test</NavLink>
     <NavLink to="/List">List</NavLink>   
     <NavLink to="/Api">Api</NavLink>
+    <NavLink to="/Newsletter">Newsletter</NavLink>
     
     </div>
   )
