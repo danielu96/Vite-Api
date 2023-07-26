9e8e13e3a61b21fe93a63733dcc075ae4792034e
Dniel K <danielu96@wp.pl>
2023-07-26 19:22:45 +0200

    login added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,6 +6,7 @@ const NavBar = () => {
     <div style={{width:'100vw',height:'5vh',border:'solid 1px gray',boxShadow:'1px 1px gray', backgroundColor:'whitesmoke',display:'flex', justifyContent:'center',gap:"1rem",paddingTop:'0.5rem' }}>
   
     <NavLink to="/">Home</NavLink>
+    <NavLink to="/login">Login</NavLink>
     <NavLink to="/tasks">Todos</NavLink>
     <NavLink to="/Test">Test</NavLink>
     <NavLink to="/List">List</NavLink>   
