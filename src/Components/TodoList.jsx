442376fe4f9331ff74d5b9fc3e464dabec15c402
Dniel K <danielu96@wp.pl>
2023-07-26 19:20:01 +0200

    author added

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import TodoCard from './TodoCard';
 
 const TodoList = ({items}) => {
 
     const formattedTodos = items.map((item)=>{
-        const {id,title,autor,email}=item
+        const {id,title,author,email}=item
         return {
             id,
             title,
-            autor,
+            author,
             email
         }
         });
@@ -16,11 +17,13 @@ const TodoList = ({items}) => {
   return (
     <div style={{marginTop:'4rem',width:"80vw",display:"grid",gridTemplateColumns:"repeat(auto-fill,minmax(300px, 1fr))",gap:'2rem' }}>
         {formattedTodos.map((item) => {
-    return (
+    return (      
+//   <Link to={`/tasks/${title}`} key={todo.id} style={{marginTop:"1rem"}} ><p>autor{todo.autor}</p><p>title:{todo.title}</p></Link>
+       
         <TodoCard key={item.id} {...item}/>
     )
 })}
-    </div>
+     </div>
   )
 }
 
