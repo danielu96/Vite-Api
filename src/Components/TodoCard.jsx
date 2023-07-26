df941be5f2927741db34b4da6911cedc0319ce7c
Dniel K <danielu96@wp.pl>
2023-07-26 19:20:27 +0200

    author added

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/todoCard'
 
-const TodoCard = ({id,title,autor,email}) => {
+const TodoCard = ({id,email,author,title}) => {
   return (
-    <div>
-    <h4> {title}</h4> 
-    <p>{autor},{email}</p>
-    <Link to={`/todo/${id}`}>details</Link>
+    <Wrapper>
+      <div className='card'>
+    <h5> {title}</h5> 
+    <p>{author}</p>
+    <p>{email}</p>
+    <Link 
+    to={`/tasks/${id}`}
+    // to={`/${title}`}
+    >details</Link>
     </div>
+    </Wrapper>
   )
 }
 
