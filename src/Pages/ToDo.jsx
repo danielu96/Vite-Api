56a0f1140b334dd0aa020305c6569dc84652f60c
Dniel K <danielu96@wp.pl>
2023-07-26 19:17:34 +0200

    data.taskList[0]; not working

--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -1,46 +1,69 @@
 import React from 'react';
 import axios from 'axios';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData,Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-const localData = 'http://localhost:5000/api/tasks' ;
+const localData = 'http://localhost:5000/api/tasks/';
 
-const singleToDoQuery = (id) => {
+// const todoDetailLoader= async ({params})=>{
+//   const {id}=params
+//   const res = fetch('http://localhost:5000/tasks/'+id)
+//   return res.json()
+// };
+
+const singleTasksQuery = (id) => {
     return {
       queryKey: ['tasks', id],
       queryFn: async () => {
-        const { data } = await axios.get(`${localData}${id}`);
-        console.log({id})
+        const { data } = await axios.get(
+          // 'http://localhost:5000/api/tasks/'+id
+          `${localData}${id}`
+          );
+        console.log({data,id})
         return data;
       },
-    };
+    }; 
   };
   
   export const loader =
     (queryClient) =>
     async ({ params }) => {
-      const { id} = params;
-      await queryClient.ensureQueryData(singleToDoQuery(id));
+      const { id} = params;     
+      await queryClient.ensureQueryData(singleTasksQuery(id));
+      console.log({singleTasksQuery})
       return { id };
     };
 
+
 const ToDo = () => {
     const { id}=useLoaderData();
-    // const { autor}=useLoaderData();
-const { data } = useQuery(singleToDoQuery(id));
-const singleToDo = data.items[0];
+    const { data } = useQuery(singleTasksQuery(id));
+    // const  {data} = useLoaderData(); 
+
+const singleToDo = 
+//  items.map((task) => {
+//   if (task.id === id) {
+//     return { ...task };
+//   }
+//   return task;
+// });
+data.taskList[0];
 const {
   
-    title:name,
-    autor:autor,
+    title:title,
+    autor:author,
    
   } = singleToDo;
   return (
-    <div>ToDo
-        {name},
-        {autor}
-      
+    <div style={{marginTop:"8rem",gap:"2rem",lineHeight:"5rem"}}>
+      <h5>ToDo</h5>
+      <p>{id}</p>
+      <p>{title}</p>
+            <p>{author}</p>
+            < Link to={'/tasks'} >back</Link>
+    
     </div>
   )
 }
+
 
 export default ToDo
