3b3e5a0fbedf97c1dd7a23680e3b1b1651cf9699
Dniel K <danielu96@wp.pl>
2023-07-26 19:29:52 +0200

    add app.get('/api/tasks/:id'

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import { nanoid } from 'nanoid';
-const app = express();
 import morgan from 'morgan';
 
+const app = express();
+
+
 let taskList = [
-  { id: nanoid(), title: 'do something impportant',autor:"DAN",email:'dan@wp.pl', isDone: false },
-  { id: nanoid(), title: 'rest some',autor:"PAUL",email:'paul@pp.pl', isDone: false },
-  { id: nanoid(), title: 'clean office',autor:"RAUL",email:'raul@wp.pl', isDone: true },
-  { id: nanoid(), title: 'take a dog with some food',autor:"JOHN",email:'john@jp.pl', isDone: false },
+  { id: nanoid(), title: 'do something important',author:"DAN",email:'dan@wp.pl', isDone: false },
+  { id: nanoid(), title: 'rest some',author:"PAUL",email:'paul@pp.pl', isDone: false },
+  { id: nanoid(), title: 'clean office',author:"RAUL",email:'raul@wp.pl', isDone: true },
+  { id: nanoid(), title: 'take a dog with some food',author:"JOHN",email:'john@jp.pl', isDone: false },
 ];
 
 if (process.env.NODE_ENV !== 'production') {
@@ -18,6 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello From Server...</h1>');
 });
@@ -25,16 +28,31 @@ app.get('/', (req, res) => {
 app.get('/api/tasks', (req, res) => {
   res.json({ taskList });
 });
+app.get('/api/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { isDone } = req.body;
+  // const { title } = req.body;
+  const { author } = req.body;
+  // const { email} = req.body;
+
+  taskList = taskList.map((task) => {
+    if (task.id === id) {
+      return { ...task , isDone,author};
+    }
+    return task;
+  });
+  res.json({ id,author });
+});
 
 app.post('/api/tasks', (req, res) => {
   const { title } = req.body;
-  const { autor } = req.body;
+  const { author } = req.body;
   const { email } = req.body;
-  if (!title && !autor && !email) {
-    res.status(400).json({ msg: 'please provide valuesa' });
+  if (!title && !author && !email) {
+    res.status(400).json({ msg: 'please provide VALUE' });
     return;
   }
-  const newTask = { id: nanoid(), title,autor,email, isDone: false };
+  const newTask = { id: nanoid(), title,author,email, isDone: false };
   taskList = [...taskList, newTask];
   res.json({ task: newTask });
 });
@@ -42,10 +60,11 @@ app.post('/api/tasks', (req, res) => {
 app.patch('/api/tasks/:id', (req, res) => {
   const { id } = req.params;
   const { isDone } = req.body;
-
+  const { title } = req.body;
+  const { author } = req.body;
   taskList = taskList.map((task) => {
     if (task.id === id) {
-      return { ...task, isDone };
+      return { ...task, isDone,title,author };
     }
     return task;
   });
