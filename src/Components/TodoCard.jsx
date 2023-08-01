4c8c097abc98fbb4ea0cb7e14d93986ab1614499
Dniel K <danielu96@wp.pl>
2023-08-01 10:43:48 +0200

    body,name added

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/todoCard'
 
-const TodoCard = ({id,email,author,title}) => {
+const TodoCard = ({id,email,body,name}) => {
   return (
     <Wrapper>
       <div className='card'>
-    <h5> {title}</h5> 
-    <p>{author}</p>
+    <h5> {name}</h5> 
+    <p>{body}</p>
     <p>{email}</p>
     <Link 
     to={`/tasks/${id}`}
