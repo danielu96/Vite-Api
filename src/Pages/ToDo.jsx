3a0be25dc209e73d430d219eab7164bade2b54d7
Dniel K <danielu96@wp.pl>
2023-08-01 10:52:22 +0200

    jsonplaceholder added

--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { useLoaderData,Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-const localData = 'http://localhost:5000/api/tasks/';
+// const localData = 'http://localhost:5000/api/tasks/${id}';
+const fakeData= "https://jsonplaceholder.typicode.com/comments?postId=1"
 
 // const todoDetailLoader= async ({params})=>{
 //   const {id}=params
@@ -12,18 +13,17 @@ const localData = 'http://localhost:5000/api/tasks/';
 
 const singleTasksQuery = (id) => {
     return {
-      queryKey: ['tasks', id],
+      queryKey: ['comments', id],
       queryFn: async () => {
         const { data } = await axios.get(
           // 'http://localhost:5000/api/tasks/'+id
-          `${localData}${id}`
+          `${fakeData}${id}`
           );
         console.log({data,id})
         return data;
       },
     }; 
-  };
-  
+  };  
   export const loader =
     (queryClient) =>
     async ({ params }) => {
@@ -46,11 +46,12 @@ const singleToDo =
 //   }
 //   return task;
 // });
-data.taskList[0];
+data[0];
 const {
   
-    title:title,
-    autor:author,
+    name:title,
+    email:email,
+    body:body,
    
   } = singleToDo;
   return (
@@ -58,12 +59,11 @@ const {
       <h5>ToDo</h5>
       <p>{id}</p>
       <p>{title}</p>
-            <p>{author}</p>
+      <p>{email}</p>
+            <p>{body}</p>          
             < Link to={'/tasks'} >back</Link>
     
     </div>
   )
 }
-
-
 export default ToDo
