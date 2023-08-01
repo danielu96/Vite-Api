d8def544eac9c42bb60a3b0d5ba6e42350af1856
Dniel K <danielu96@wp.pl>
2023-08-01 10:51:38 +0200

    action added

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -2,38 +2,62 @@ import React from 'react';
 import {  useState } from 'react';
 import Wrapper from '../assets/wrappers/App';
 import { ToastContainer } from 'react-toastify';
-import { Link } from 'react-router-dom';
+import { Link,Form,useNavigation,redirect  } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const initialState= {
-  name:'',
-  email:'',
-  password:'',
-  isMember:true,
+const UsersUrl = 'http://localhost:5000/api/users';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await axios.post(UsersUrl, data);
+
+    toast.success(response.data.msg);
+    return redirect('/users');
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
 
 
+
+
+// const initialState= {
+//   name:'',
+//   email:'',
+//   password:'',
+//   isMember:true,
+// };
+
+
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   // const [values,setValues]=useState(initialState);
-    const [name , setName] = useState('');  
-    const [autor , setAutor] = useState('');  
-    const [email , setEmail] = useState('');  
-    const [password , setPassword] = useState('');  
-    const [confirmPassword , setConfirmPassword] = useState('');  
+    // const [name , setName] = useState('');      
+    // const [email , setEmail] = useState('');  
+    // const [password , setPassword] = useState('');  
+    // const [confirmPassword , setConfirmPassword] = useState('');  
 
     // const handleChange = (e)=> {
     //   const name= e.target.name;
     //   const value= e.target.value;
     // }
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault(); 
-        createTask(name && email, {
-          onSuccess: () => {
-            setName('');
-            setEmail('');
-          },
-        });
-    }
+    // const handleSubmit = async (e) =>{
+    //     e.preventDefault(); 
+    //     createTask(name && email, {
+    //       onSuccess: () => {
+    //         setName('');
+    //         setEmail('');
+    //       },
+    //     });
+    // }
   return (
     <>
     <Wrapper>      
@@ -42,44 +66,47 @@ const Register = () => {
      />
     <div className='container'>
         <h3>Register</h3>
-    <form style={{display:'grid',alignContent:'center', justifyContent:'center'}}
+    <Form method='POST'style={{display:'grid',alignContent:'center', justifyContent:'center'}}
     // onSubmit={(e)=>setTitle( e.target.value)}
     >
           <label style={{margin:'auto auto 7px 0px'}} htmlFor='title'>Name</label>     
       <input 
       style={{height:"1.3rem",background:'#f7f7f7',borderRadius:"5px",border:"none",marginBottom:"5px"}}
       type='text'
+      name='name'
       id='name'
-      value={name}
-      onChange={(e)=>setName( e.target.value)}
+      // value={name}
+      // onChange={(e)=>setName( e.target.value)}
       ></input> 
       <label style={{height:"1.3rem",margin:'auto auto 1px 1px '}} htmlFor='title'>Email</label>     
       <input 
       style={{height:"1.3rem",background:'#f7f7f7',borderRadius:"5px",border:"none",marginBottom:"5px"}}
       type='email'
+      name='email'
       id='email'
-      value={email}
-      onChange={(e)=>setEmail( e.target.value)}
+      // value={email}
+      // onChange={(e)=>setEmail( e.target.value)}
       ></input> 
        <label style={{margin:'auto auto 7px 0px'}} htmlFor='autor'>Password</label>
      <input 
       style={{height:"1.3rem",background:'#f7f7f7',borderRadius:"5px",border:"none",marginBottom:"5px"}}
       type='password'
+      name='password'
       id='password'
-      value={password}
-      onChange={(e)=>setPassword( e.target.value)}
+      // value={password}
+      // onChange={(e)=>setPassword( e.target.value)}
       ></input>   
-       <label style={{margin:'auto auto 7px 0px'}} htmlFor='autor'>Confirm Password</label>
+       {/* <label style={{margin:'auto auto 7px 0px'}} htmlFor='autor'>Confirm Password</label>
      <input 
       style={{height:"1.3rem",background:'#f7f7f7',borderRadius:"5px",border:"none"}}
       type='password'
       id='confirmPassword'
       value={confirmPassword}
       onChange={(e)=>setConfirmPassword( e.target.value)}
-      ></input>           
-   <button onClick={handleSubmit}>Sign Up</button>
+      ></input>            */}
+   <button type='submit' disabled={isSubmitting}> {isSubmitting ? 'submitting' : 'submit'}</button>
   <div style={{marginTop:"1rem"}}> Already have an acount? <Link to={'/login'}>Login Here</Link></div>
-     </form>      
+     </Form>      
     </div>    
    
     </Wrapper>
