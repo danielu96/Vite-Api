6473775b82509a31c5cd436b26920cbf38317d2d
Dniel K <danielu96@wp.pl>
2024-01-13 16:33:36 +0100

    axios.jsx created

---
+++ src/UTILS/axios.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+import { clearStore } from '../Pages/FEATURES/USER/userSlice';
+import { getUserFromLocalStorage } from './localStorage';
+
+export const JobsFetch = axios.create({ 
+  baseURL:  'http://localhost:5000/api/jobs/' ,
+  // baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+});
+const customFetch = axios.create({ 
+  baseURL:  'http://localhost:5000/api/users' ,
+  // baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+});
+
+
+// customFetch.interceptors.request.use((config) => {
+//   const user = getUserFromLocalStorage();
+//   if (user) {
+//     config.headers['Authorization'] = `Bearer ${user.token}`;
+//   }
+//   return config;
+// });
+// JobsFetch.interceptors.request.use((config) => {
+//   const user = getUserFromLocalStorage();
+//   if (user) {
+//     config.headers['Authorization'] = `Bearer ${user.token}`;
+//   }
+//   return config;
+// });
+
+export const checkForUnauthorizedResponse = (error, thunkAPI) => {
+  if (error.response.status === 401) {
+    thunkAPI.dispatch(clearStore());
+    return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+  }
+  return thunkAPI.rejectWithValue(error.response.data.msg);
+};
+
+export default customFetch;
