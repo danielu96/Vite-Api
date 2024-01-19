817328e477003a3e2e8978f76934aaad5d3da00e
Dniel K <danielu96@wp.pl>
2024-01-19 14:31:35 +0100

    CoctailList added

---
+++ src/Components/CoctailList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/Test';
+import CoctailCard from './CoctailCard';
+
+const CoctailList = ({drinks}) => {
+
+const formattedDrinks = drinks.map((item)=>{
+const {idDrink,strDrink,strGlass}=item
+return {
+    id: idDrink,
+    name:strDrink,
+    glass:strGlass
+}
+});
+
+
+  return (
+    <Wrapper>    
+        
+{formattedDrinks.map((item) => {
+    return (
+        <CoctailCard key={item.id} {...item}/>
+    )
+})}    
+     </Wrapper>
+  )
+}
+
+export default CoctailList
