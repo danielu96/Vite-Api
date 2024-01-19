b3e64874f6207111d06dedc110364fab470bb223
Dniel K <danielu96@wp.pl>
2024-01-19 14:51:19 +0100

    added mockUserList

---
+++ src/Mocks/mockData.js
@@ -0,0 +1,34 @@
+
+ export const  mockUserList = [
+      {
+        userId: 1,
+        id: "G3ImDzDxpb2dNSPH7sBIn",
+        password: "123",
+        name: "daniel",
+        lastName: "Karka",
+        location: "wasa",
+        email: "daniel@wp.pl"
+      },
+      {
+        userId: 2,
+        id: "CxumneqBni5GXuSlKG1WP",
+        password: "pa213",
+        name: "PAUL",
+        email: "paul@pp.pl"
+      },
+      {
+        userId: 3,
+        id: "cBogya9KGFeO9OBatNzDl",
+        password: "r098",
+        name: "RAUL",
+        email: "raul@wp.pl"
+      },
+      {
+        userId: 4,
+        id: "oTR_0Tu8vttdSLELa_b7b",
+        password: "j456",
+        name: "JOHN",
+        email: "john@jp.pl"
+      }
+    ]
+  
