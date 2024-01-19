d5bbeb2db9b7c106afce2c2d13fe4d16e8908c73
Dniel K <danielu96@wp.pl>
2024-01-19 15:03:53 +0100

    added items.taskList.map

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -3,29 +3,32 @@ import TodoList from '../Components/TodoList'
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 const localData = 
-// 'http://localhost:5000/api/tasks'
-"https://jsonplaceholder.typicode.com/comments"
-// "../DATA/db.json"
- ;
-
-export const loader = async ()=>{
-    // const searchTerm="";
-    const response= await axios.get   (`${localData}`)
-   
-   
-    ;
+'http://localhost:5000/api/tasks' ;
+export const loader = async ()=>{   
+    const response= await axios.get   (`${localData}`);
     console.log(response)
     return {items:response.data}
   };
-
-
 const Todos = () => {
     const {items}=useLoaderData();
     console.log(items);
   return (
-    <div >
-        <TodoList items={items}/>
+    <>
+   <div>
+    <h3 style={{marginBottom:"1rem"}}>Info from Todos</h3>
+    <p>Your Tasks: {items.taskList.length}</p>
+   
+{items.taskList.map((item)=>{
+  return(
+    <div key={item.id}>
+      <h5>{item.title}</h5>,
+      <p>{item.author}</p> 
     </div>
+  )
+})}
+   </div>   
+   <TodoList/>        
+ </>
   )
 }
 
