b36cf0dd68239ed0bc74e4ce65c2951362b4bbde
Dniel K <danielu96@wp.pl>
2024-01-19 14:50:31 +0100

    added Items

--- src/Pages/Newsletter.jsx
+++ src/Pages/Newsletter.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Form,useNavigation,redirect } from 'react-router-dom'
+import { Form,useNavigation,redirect, useLoaderData } from 'react-router-dom'
 import axios from 'axios';
 import { toast } from 'react-toastify';
+// import Stat from '../Components/Stat';
+import { QueryClient } from '@tanstack/react-query';
+import customFetch from '../Components/utils';
+import Items from '../Components/Items';
+import { useFetchTasks } from '../Components/ReactQueryCustomHooks';
 
 const newsletterUrl = 'http://localhost:5000/api/tasks';
 
@@ -20,11 +25,33 @@ export const action = async ({ request }) => {
     return error;
   }
 };
+// const localData = 
+// 'http://localhost:5000/api/tasks'
+// // "https://jsonplaceholder.typicode.com/comments"
+// // "../DATA/db.json"
+//  ;
+
+// export const loader = async ()=>{
+//   // const searchTerm="";
+//   // const response= await axios.get   (`${localData}`)
+//  const response = await customFetch.get('/')
+ 
+//   ;
+//   console.log(response)
+//   return {items:response.data.taskList}
+// };
 
 const Newsletter = () => {
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
+    const {data} = useFetchTasks()
+    console.log(data)
   return (
+    <>
+
+    <div>
+      
+    </div>
     <Form className='form' method='POST'>
       <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
         our newsletter
@@ -77,9 +104,11 @@ const Newsletter = () => {
       >
         {isSubmitting ? 'submitting' : 'submit'}
       </button>
+      
     </Form>
-
-
+    {/* <div>{data.data.length}</div> */}
+<Items/>
+</>
   )
  
   
