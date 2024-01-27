ac47c0b4e3009ff3defe8d890aa24e40ca250b43
Dniel K <danielu96@wp.pl>
2024-01-27 19:24:03 +0100

    MockData in tests directiory

---
+++ __tests__/MockData.jsx
@@ -0,0 +1,37 @@
+import { nanoid } from 'nanoid';
+
+  const  UserList = [
+      {
+        id: nanoid(),
+        password: "123",
+        name: "Daniel",
+        lastName: "Kartkowski",
+        location: "Wasabi",
+        email: "daniel@wp.pl"
+      },
+      {
+        id:nanoid(),
+        password: "pa213",
+        name: "Paul",
+        lastName: "Polowski",       
+        location: "San Paulo",
+        email: "paul@pp.pl",
+      },
+      {
+        id: nanoid(),
+        password: "r098",
+        name: "Raulo",
+        lastName: "Raulenios", 
+        location: "RaulosTown",
+        email: "raul@wp.pl"
+      },
+      {
+        id: nanoid(),
+        password: "j456",
+        name: "John",
+        lastName: "Johnas", 
+        location: "JohnTown",
+        email: "john@jp.pl"
+      }
+    ]
+  export default UserList
