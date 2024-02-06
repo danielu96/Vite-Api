f29f33c009054f270a626f4cdcc478da56b3035a
Dniel K <danielu96@wp.pl>
2024-02-06 16:59:12 +0100

    add NavLinks

---
+++ src/Components/NavLinks.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+const links = [
+    { id: 1, url: '/', text: 'home' },
+    { id: 2, url: 'login', text: 'login' },
+    { id: 3, url: 'users', text: 'users' },
+    { id: 4, url: 'comments', text: 'comments' },
+    { id: 5, url: 'tasks', text: 'tasks' },
+    { id: 6, url: 'test', text: 'test' },
+    { id: 7, url: 'list', text: 'list' },
+    { id: 8, url: 'apiks', text: 'apiks' },
+    { id: 9, url: 'newsletter', text: 'newsletter' },
+    { id: 10, url: 'stat', text: 'stat' },
+  ];
+  
+  
+  const NavLinks = () => {
+       
+    return (
+      <>
+      {links.map((link) => {
+        const { id, url, text } = link;       
+        return (
+          <li key={id}>
+            <NavLink className='capitalize' to={url}>
+              {text}
+            </NavLink>
+          </li>
+        );
+      })}
+    </>
+    
+    );
+  };
+  export default NavLinks;
