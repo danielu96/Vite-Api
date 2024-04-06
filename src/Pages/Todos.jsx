aab40a11e54ec188854c2dd421fb287ecaa70501
Dniel K <danielu96@wp.pl>
2024-04-06 14:44:32 +0200

    add loader

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,21 +1,66 @@
 import React from 'react'
-// import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Form,useNavigation,redirect } from 'react-router-dom'
 import FormInput from '../Components/FormInput';
 import Items from '../Components/Items';
-// import TodoList from '../Components/TodoList';
+import customFetch from '../Components/utils';
+import TodoList from '../Components/TodoList';
+
+
+export const tasksQuery = (params) => {
+  return {
+    queryKey: [
+      'tasks'          
+    ],
+    queryFn: () =>
+      customFetch.get('/', {
+        params,        
+            
+      }),      
+  };  
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+   
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await queryClient.ensureQueryData(
+        tasksQuery(params)
+      );
+
+      return {
+        items: response.data,
+        // totalMessages:response.data.totalMessages        
+      };
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'there was an error accessing your contact';
+
+      toast.error(errorMessage);
+      if (error?.response?.status === 401 || 403) return redirect('/tasks');
+      return null;
+    }
+  };
 
 const tasksUrl = 'http://localhost:5000/api/tasks';
 
-export const action = async ({ request }) => {
+export const action = 
+( queryClient) =>
+async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
   try {
     const response = await axios.post(tasksUrl, data);
+    queryClient.removeQueries(['taskyos']); 
     toast.success(response?.data?.msg);
     return redirect('/tasks');
   } catch (error) {
@@ -28,15 +73,14 @@ export const action = async ({ request }) => {
 
 
 
-const localData = 
-'http://localhost:5000/api/tasks' ;
-export const loader = async ()=>{   
-    const response= await axios.get   (`${localData}`);
-    console.log(response)
-    return {items:response.data}
-  };
-const Todos = () => {
-  // const [selectedUser, setSelectedUser] = useState(null);
+// const localData = 
+// 'http://localhost:5000/api/tasks' ;
+// export const loader = async ()=>{   
+//     const response= await axios.get   (`${localData}`);
+//     console.log(response)
+//     return {items:response.data}
+//   };
+const Todos = () => {  
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';  
     const {items}=useLoaderData();
@@ -59,15 +103,16 @@ const Todos = () => {
       <FormInput type='email' name='email'  label='email address'/>  
       </div>     
       <button
-        type='submit'
+        type='submit'        
         className='btn btn-primary btn-block'       
         disabled={isSubmitting}
       >
-        {isSubmitting ? 'submitting' : 'submit'}
+        {isSubmitting ? 'submitting' : 'submit'}       
       </button>      
     </Form>           
 </div> 
-<Items/>
+<Items/> 
+<TodoList/>
  </>
   )
 }
