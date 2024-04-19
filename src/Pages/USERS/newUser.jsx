5a1c949eb710b09d6a9a25bfc51883b12b104b43
Dniel K <danielu96@wp.pl>
2024-04-19 12:27:42 +0200

    add new url

--- src/Pages/USERS/newUser.jsx
+++ src/Pages/USERS/newUser.jsx
@@ -1,36 +1,68 @@
+// import { useSelector,useDispatch } from 'react-redux'
 import { useEffect, useState } from "react";
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { NewUserDetail } from "./newUserDetail";
-
-const maxPostPage = 5;
+// import Pagination from "../../Components/Pagination";
+// import ReactPaginate from 'react-paginate';
+const maxPostPage = 4;
 
 async function fetchPosts(pageNum) {
   const response = await fetch(
-     `http://localhost:5000/api/users?${pageNum}`
+     `http://localhost:5000/api/jobs?${pageNum}`
   );
   return response.json();
 }
 
 export function NewUser() {
+  // const  userList = useSelector((state) => state.userList);
+  // const [currentItems, setCurrentItems] = useState([]); 
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedUser, setSelectedUser] = useState(null);
+  // const [pageCount, setPageCount] = useState(0);
+  // const [itemOffset, setItemOffset] = useState(0);
+  // const itemsPerPage = 3;
 
   const queryClient = useQueryClient();
 
+  // const handlePageClick = (event) => {
+  //   const newOffset = (event.selected * itemsPerPage) %
+  //     userList.length;
+  //   console.log(
+  //     `User requested page number ${event.selected}, which is offset ${newOffset}`
+  //   );
+  //   setItemOffset(newOffset);
+  // }; 
+
+
+  // useEffect(() => {
+  //   if (currentPage < maxPostPage) {
+  //     const nextPage = currentPage + 1;
+  //     queryClient.prefetchQuery(["users", nextPage], () =>
+  //       fetchPosts(nextPage)
+  //     );
+  //   }
+  // }, [currentPage, queryClient]);
+  // useEffect(() => {    
+  //   const endOffset = itemOffset + itemsPerPage;
+  //   setCurrentItems(data.userList.slice(itemOffset, endOffset));
+  //   setPageCount(Math.ceil(userList.length / itemsPerPage));
+  // }, [itemOffset, itemsPerPage
+    
+  // ]);
+
+
   useEffect(() => {
-    if (currentPage < maxPostPage) {
-      const nextPage = currentPage + 1;
-      queryClient.prefetchQuery(["users", nextPage], () =>
-        fetchPosts(nextPage)
-      );
-    }
-  }, [currentPage, queryClient]);
+  const nextPage = currentPage + 1;
+  if (nextPage <= maxPostPage) {
+    queryClient.prefetchQuery(["users", nextPage], () => fetchPosts(nextPage));
+  }
+}, [currentPage, queryClient]);
 
   const { data, isError, error, isLoading } = useQuery(
     ["users", currentPage],
     () => fetchPosts(currentPage),
     {
-      staleTime: 2000,
+      staleTime: 200,
       keepPreviousData: true,
     }
   );
@@ -45,39 +77,65 @@ export function NewUser() {
 
   return (
     <>
-   <h1>USERS</h1> 
-      <ul>
-        {data.userList.map((user) => (
-          <li
+    <div className="-mt-4 grid grid-cols-2 mx-5 border-solid border-2 w-auto">
+      <div className="ml-20 ">
+   <h1 className="mb-5 font-bold"   >USERS</h1> 
+      <ul className="cursor-pointer">
+        {data.jobs.map((user) => (
+          <li className="hover:font-medium hover:text-primary"
             key={user.id}
-            className="post-title"
+          
             onClick={() => setSelectedUser(user)}
           >
-          name:  {user.name},<br/>       
+          name:  {user.jobType}     
           </li>
+          
         ))}
       </ul>
-      <div className="pages">
+      
+      <div className="pages mx-auto my-3">
         <button
           disabled={currentPage <= 1}
           onClick={() => {
             setCurrentPage((previousValue) => previousValue - 1);
           }}
         >
-          Previous page
+          Prev
         </button>
-        <span>Page {currentPage}</span>
+        <span className="font-bold"> {currentPage} </span>
         <button
           disabled={currentPage >= maxPostPage}
           onClick={() => {
             setCurrentPage((previousValue) => previousValue + 1);
           }}
         >
-          Next page
+           Next
         </button>
+
       </div>
-      <hr />
+      {/* <Pagination/> */}
+      </div>
+      {/* <div style={{paddingLeft:'0rem'}}>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< prev"
+        renderOnZeroPageCount={null}
+        containerClassName={"pagination"}
+        pageLinkClassName={"page-num"}
+        previousLinkClassName={"page-num"}
+        nextLinkClassName={"page-num"}
+        disabledClassName={"disabled"}
+        activeClassName={"active"}
+
+      />
+      </div> */}
+     
       {selectedUser && <NewUserDetail user={selectedUser} />}
+      </div>
     </>
   );
 }
