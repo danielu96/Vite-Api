924d603f8f7624bee7805ed4ac85b3c076a679a6
Dniel K <danielu96@wp.pl>
2024-05-09 12:42:23 +0200

    add deleteAllAddresses

--- server/routes/newsletterRouter.js
+++ server/routes/newsletterRouter.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 
-import { createNewsletter, getAllNewsletter } from'../controllers/newsletterController.js';
+import { createNewsletter,deleteAddress, getAllNewsletter,deleteAllAddresses } from'../controllers/newsletterController.js';
 
 
 
 const router = Router();  
 router.post('/', createNewsletter)
 router.get('/',getAllNewsletter)
+router.delete('/',deleteAllAddresses)
+router.route('/:id').delete(deleteAddress)
 export default router;
