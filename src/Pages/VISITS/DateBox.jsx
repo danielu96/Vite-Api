c581ecdc856a06d77af9027a5907fc69fb569aa2
Dniel K <danielu96@wp.pl>
2024-05-29 22:39:02 +0200

    display appointments in the same day

--- src/Pages/VISITS/DateBox.jsx
+++ src/Pages/VISITS/DateBox.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import { useLoaderData } from 'react-router-dom';
+import { redirect,Form } from 'react-router-dom';
+import  {FormInput,SubmitBtn} from '../../Components';
+import { useState } from 'react';
+// const [selectedTime, setSelectedTime] = useState('');
 
 const isToday = (day) => {
   // Create a Dayjs object for the current date
@@ -15,9 +19,37 @@ const DateBox = ({ date, isSunday,monthYear,hasAppointment,appointment }) => {
   const {data}=useLoaderData();  
   console.log(data); 
 
-  const names = data.map(appointment => appointment.name);
-  console.log(names); // Array containing all names
+  // const createCustomStep = (minStep) => {
+  //   const steps = [];
+  //   for (let hour = 10; hour <= 18; hour++) {
+  //     for (let minute = 0; minute < 60; minute += minStep) {
+  //       const formattedTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+  //       steps.push(formattedTime);
+  //     }
+  //   }
+  //   return steps;
+  // };
+  const validateTime = (time) => {
+    const selectedTime = dayjs(time, 'HH:mm'); // Parse the time string
+    const isValidHour = selectedTime.hour() >= 10 && selectedTime.hour() <= 18; // Check if hour is between 10 and 18
+    if (!isValidHour) {
+      return 'Please select a time between 10:00 and 18:00.'; // Return error message
+    }
+    return true; // Time is valid
+  };
+
+
+  
+  const authors = data.map(appointment => appointment.author); 
+  const appointments = data;
+  console.log(appointments);
+  console.log(authors); // Array containing all names
+  const filteredAppointments = appointments.filter(
+    (appointment) => dayjs(`${monthYear.year}-${monthYear.month}-${date}`).isSame(dayjs(appointment.date), 'date')
+  );
   return (
+
+    
     <div
       className={
         isTodayOfDate
@@ -27,16 +59,48 @@ const DateBox = ({ date, isSunday,monthYear,hasAppointment,appointment }) => {
       }
     >
       <div className="text-center pt-4 pb-4 grid">      
-         <div className="text-center pt-4 pb-4 grid">
+         <div className="text-center pt-4 pb-4 grid overflow-y-scroll ">
         {date}
-        {isTodayOfDate && <span>we have : {dayjs().format('DD.MM.YYYY')}</span>}
-        {hasAppointment && appointment && ( // Check both conditions
-          <>
-            <span className="text-green-500 font-bold">Appointment:</span>
-            <span className="text-gray-600 font-bold">{appointment.name}</span>
-          </>
-        )}
-      </div>
+        {isTodayOfDate && <span>we have : {dayjs().format('DD.MM.YYYY')}</span>}    
+          {hasAppointment && filteredAppointments.length > 0 && (
+  <>
+    <span className="text-green-500 font-bold">Appointments:</span>
+    <ul className="list-disc pl-4 mt-2">
+      {filteredAppointments.map((appointment) => (
+        <li key={appointment.id}>
+          {appointment.author} - {appointment.time}
+        </li>
+      ))}
+    </ul>
+  </>
+)}       
+      </div>    
+            {isSunday ? null : <Form  method='POST'className='flex flex-col gap-y-4'>     
+       <div className='mt-4'>
+
+        
+       <FormInput type='text' name='author'   />      
+       <FormInput type='date' name='date'   />    
+       <FormInput type='time' name='time' step="30"   />    
+       
+        {/* <FormInput type='time' name='time' step="1800"   /> */}
+        {/* <FormInput
+  type='time'
+  name='time'
+  step='30'
+  onChange={(e) => {
+    const error = validateTime(e.target.value); // Validate time on change
+    if (error) {
+      // Set error state or display error message
+    } else {
+      // Time is valid, update state or perform other actions
+    }
+  }}
+/> */}
+{/* <FormInput type="time" id="myTime" min="10:00:00" max="18:00:00" step="60"/> */}
+
+      </div>      
+    </Form>  }
       </div>
  <div className="text-center pt-4 pb-4 grid">          
             </div>
