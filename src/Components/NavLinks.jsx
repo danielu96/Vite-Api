2f9f8767278928ab5fc1ef9ba042c515102342fa
Dniel K <danielu96@wp.pl>
2024-06-04 14:52:29 +0200

    delete login from menu

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -1,16 +1,15 @@
 import { NavLink } from 'react-router-dom';
 const links = [
-    { id: 1, url: '/', text: 'home' },
-    { id: 2, url: 'login', text: 'login' },
-    { id: 3, url: 'users', text: 'users' },
-    { id: 4, url: 'jobs', text: 'jobs' },
-    { id: 5, url: 'tasks', text: 'tasks' },
-    { id: 6, url: 'test', text: 'test' },   
-    { id: 7, url: 'newsletter', text: 'newsletter' },
-    { id: 8, url: 'stat', text: 'stat' },
-    { id: 9, url: 'calendar', text: 'calendar' },
-    { id: 10, url: 'appointments', text: 'appointments' },
-    { id: 11, url: 'visits', text: 'visits' },
+    { id: 1, url: '/', text: 'home' },    
+    { id: 2, url: 'users', text: 'users' },
+    { id: 3, url: 'jobs', text: 'jobs' },
+    { id: 4, url: 'tasks', text: 'tasks' },
+    { id: 5, url: 'test', text: 'test' },   
+    { id: 6, url: 'newsletter', text: 'newsletter' },
+    { id: 7, url: 'stat', text: 'stat' },
+    { id: 8, url: 'calendar', text: 'calendar' },
+    { id: 9, url: 'appointments', text: 'appointments' },
+    { id: 10, url: 'visits', text: 'visits' },
   ];
   
   
