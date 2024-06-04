9c846241d4f251db6ca165e8c55167fda39e1e2b
Dniel K <danielu96@wp.pl>
2024-06-04 15:06:02 +0200

    delete StrictMode add 2 imports toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import  store  from './store';
+import  {store}  from './store';
 import { Provider } from 'react-redux';
 import './index.css'
-import { ToastContainer } from 'react-toastify';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-      <Provider store={store}>
-    <App />
-    </Provider>    
-    <ToastContainer position='top-center' />
-    </React.StrictMode>
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//  <React.StrictMode>
+//       <Provider store={store}>
+//     <App />
+//     </Provider>    
+//     <ToastContainer position='top-center' />
+//     </React.StrictMode>
    
+// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer position='top-center' />
+    </Provider>
+ ,
 )
