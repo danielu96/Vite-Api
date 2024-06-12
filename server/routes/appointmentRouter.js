bf6b450d3ad6bad94b0257107501523213641887
Dniel K <danielu96@wp.pl>
2024-06-12 11:57:58 +0200

    :year/:month/:day added

--- server/routes/appointmentRouter.js
+++ server/routes/appointmentRouter.js
@@ -7,6 +7,6 @@ const router = Router();
 router.post('/', createAppointment)
 router.get('/',getAllAppointments)
 router.get('/:id',getAppointmentById)
-router.get('/:date',getAppointment)
+router.get('/:year/:month/:day',getAppointment)
 router.delete('/:id',deleteAppointment)
 export default router;
