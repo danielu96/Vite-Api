0501f00d1037cad548dec12008ca0db3e64465db
Dniel K <danielu96@wp.pl>
2024-06-14 12:33:39 +0200

    authenticateUser added

--- server/routes/appointmentRouter.js
+++ server/routes/appointmentRouter.js
@@ -1,10 +1,10 @@
 import {Router} from 'express';
 import { createAppointment, deleteAppointment, getAllAppointments, getAppointment, getAppointmentById } from '../controllers/appointmentController.js';
-
+import {authenticateUser} from '../middleware/authMiddleware.js' 
 
 
 const router = Router();  
-router.post('/', createAppointment)
+router.post('/', authenticateUser, createAppointment)
 router.get('/',getAllAppointments)
 router.get('/:id',getAppointmentById)
 router.get('/:year/:month/:day',getAppointment)
